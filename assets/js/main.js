c3a816c556fc6044d98b25cfae5c3ed86b71eb96
MasoumehZamani <zamani.masoumeh1991@gmail.com>
2025-02-21 18:45:02 +0330

    feat: Add search functionality with overlay

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,25 @@
 const menuList = document.querySelector(".menu");
 const toggle = document.querySelector(".toggle");
+const overlay = document.querySelector(".overlay");
+
+const searchForm = document.querySelector(".search-form");
 
 function toggleMenu() {
   menuList.classList.toggle("active");
 
   if (menuList.classList.contains("active")) {
-    toggle.innerHTML = `<i class="ri-close-fill"></i>`;
+    toggle.innerHTML = '<i class="ri-close-fill"></i>';
   } else {
-    toggle.innerHTML = `<i class="ri-menu-line"></i>`;
+    toggle.innerHTML = '<i class="ri-menu-line"></i>';
   }
+}
+
+function showSearch() {
+  overlay.classList.add("show-overlay");
+  searchForm.classList.add("active");
+}
+
+function closeOverlay() {
+  overlay.classList.remove("show-overlay");
+  searchForm.classList.remove("active");
 }
