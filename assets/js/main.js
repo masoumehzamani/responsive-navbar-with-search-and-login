ca082c997623a31a1d379144a71d0af07db43002
MasoumehZamani <zamani.masoumeh1991@gmail.com>
2025-02-22 12:44:49 +0330

    feat: add responsive login form

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,6 +3,7 @@ const toggle = document.querySelector(".toggle");
 const overlay = document.querySelector(".overlay");
 
 const searchForm = document.querySelector(".search-form");
+const loginForm = document.querySelector(".login-form");
 
 function toggleMenu() {
   menuList.classList.toggle("active");
@@ -17,9 +18,17 @@ function toggleMenu() {
 function showSearch() {
   overlay.classList.add("show-overlay");
   searchForm.classList.add("active");
+  loginForm.classList.remove("active");
+}
+
+function showForm() {
+  overlay.classList.add("show-overlay");
+  loginForm.classList.add("active");
+  searchForm.classList.remove("active");
 }
 
 function closeOverlay() {
   overlay.classList.remove("show-overlay");
   searchForm.classList.remove("active");
+  loginForm.classList.remove("active");
 }
